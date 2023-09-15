9af57b981807e881d597b06938c0237549bffbce
Артём Басман <steamakk47293@gmail.com>
2023-09-15 12:24:13 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+      <!DOCTYPE html>
+      <html lang="ru">
+      <head>
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <title>Basman Artem. lab 1.</title>
+      </head>
+      <body>
+         <header>
+            НГТУ, ФБ, Лабораторная работа № 1
+         </header>
+
+         <h1>web-сервер на flask</h1>
+         
+         <footer>
+            &copy; Басман Артём, ФБИ-12, 3 курс, 2023
+         </footer>
+
+      </body>
+      </html>
+ """
