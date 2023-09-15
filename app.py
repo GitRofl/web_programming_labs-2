57a36a35d80f032f30ea4e42052ab9a36e931ae6
Артём Басман <steamakk47293@gmail.com>
2023-09-15 12:50:57 +0700

    Меню

--- app.py
+++ app.py
@@ -2,8 +2,32 @@ from flask import  Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
+      <!DOCTYPE html>
+      <html lang="ru">
+      <head>
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <title>НГТУ, ФБ, Лабораторные работы</title>
+      </head>
+      <body>
+         <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+         </header>
+            <a href="/lab1">Первая лабораторная</a>
+         <footer>
+            &copy; Басман Артём, ФБИ-12, 3 курс, 2023
+         </footer>
+
+      </body>
+      </html>
+ """
+ 
+@app.route("/lab1")
+def lab1():
+   return """
       <!DOCTYPE html>
       <html lang="ru">
       <head>
@@ -17,11 +41,17 @@ def start():
          </header>
 
          <h1>web-сервер на flask</h1>
-         
+         <div>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+         </div>
          <footer>
             &copy; Басман Артём, ФБИ-12, 3 курс, 2023
          </footer>
 
       </body>
       </html>
- """
+   """
