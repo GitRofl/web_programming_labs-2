0408f391639b87800e204494ab97cd505274fc80
Артём Басман <steamakk47293@gmail.com>
2023-09-15 14:19:47 +0700

    Роуты

--- app.py
+++ app.py
@@ -15,15 +15,16 @@ def menu():
          <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <title>НГТУ, ФБ, Лабораторные работы</title>
+         <link rel="stylesheet" href=" """ + url_for('static', filename='lab1.css') +  """" ">
       </head>
       <body>
-         <header>
+         <header class="xd">
             НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
          </header>
          
          <a href="/lab1">Первая лабораторная</a>
          
-         <footer>
+         <footer class="xd">
             &copy; Басман Артём, ФБИ-12, 3 курс, 2023
          </footer>
 
@@ -40,9 +41,10 @@ def lab1():
          <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <title>Basman Artem. lab 1.</title>
+         <link rel="stylesheet" href=" """ + url_for('static', filename='lab1.css') +  """" ">
       </head>
       <body>
-         <header>
+         <header class="xd">
             НГТУ, ФБ, Лабораторная работа № 1
          </header>
 
@@ -54,7 +56,17 @@ def lab1():
             называемых микрофреймворков — минималистичных каркасов
             веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
          </div>
-         <footer>
+         <a href="/menu">Меню</a>
+         
+         <h2>Реализованные роуты</h2>
+         <ul>
+            <li> <a href="/lab1/oak">Дуб</a> </li>
+            <li> <a href="/lab1/student">Студент</a> </li>
+            <li> <a href="/lab1/python">Питон</a> </li>
+            <li> <a href="/lab1/ogr">Шрек</a> </li>
+         </ul>
+         
+         <footer class="xd">
             &copy; Басман Артём, ФБИ-12, 3 курс, 2023
          </footer>
 
@@ -74,8 +86,83 @@ def oak():
       <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') +  '''">
    </head>
    <body>
+   
+      <a href="/lab1">Назад</a>
+   
       <h1>Дуб</h1>
+     
       <img src="''' + url_for('static', filename='oak.jpg') + '''">
+   </body>
+   </html>
+'''
+@app.route("/lab1/student")
+def student():
+   return '''
+   <!DOCTYPE html>
+   <html lang="ru">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Student</title>
+      <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') +  '''">
+   </head>
+   <body>
+      <a href="/lab1">Назад</a>
+      <h1>Басман Артем Игоревич</h1>
+      <img class="log" src="''' + url_for('static', filename='nstu.jpg') + '''">
+   </body>
+   </html>
+'''
+
+@app.route("/lab1/python")
+def python():
+   return '''
+   <!DOCTYPE html>
+   <html lang="ru">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Python</title>
+      <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') +  '''">
+   </head>
+   <body>
+      <a href="/lab1">Назад</a>
+      <h1>Python</h1>
+      <div class="p"> Python — это язык программирования, который широко используется в интернет-приложениях,
+      разработке программного обеспечения, науке о данных и машинном обучении (ML). Разработчики используют Python,
+      потому что он эффективен, прост в изучении и работает на разных платформах. Программы на языке
+      Python можно скачать бесплатно, они совместимы со всеми типами систем и повышают скорость разработки. </div>
+      <img src="''' + url_for('static', filename='pyt.jpg') + '''">
+   </body>
+   </html>
+'''
+
+@app.route("/lab1/ogr")
+def ogr():
+   return '''
+   <!DOCTYPE html>
+   <html lang="ru">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Shrek</title>
+      <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') +  '''">
+   </head>
+   <body>
+      <a href="/lab1">Назад</a>
+      <h1>Шрек</h1>
+      <div class="p">
+         Жил да был в сказочном государстве большой зеленый великан по имени Шрэк.
+         Жил он в гордом одиночестве в лесу, на болоте, которое считал своим.
+         Но однажды злобный коротышка — лорд Фаркуад, правитель волшебного королевства,
+         безжалостно согнал на Шрэково болото всех сказочных обитателей.
+      </div>
+      <div class="p">
+         И беспечной жизни зеленого великана пришел конец. Но лорд Фаркуад пообещал вернуть
+         Шрэку болото, если великан добудет ему прекрасную принцессу Фиону, которая томится
+         в неприступной башне, охраняемой огнедышащим драконом.
+      </div>
+      <img class="sh" src="''' + url_for('static', filename='org.jpg') + '''">
    </body>
    </html>
 '''
