4f8ff76f2ff2d92378fec3b194491f2f2843f5d1
Артём Басман <steamakk47293@gmail.com>
2023-09-17 23:47:08 +0700

    имя студента

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import  Flask, redirect, url_for
+from flask import  Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
     return redirect("/menu", code=302)
+
+@app.route("/lab2/example")
+def example():
+   name= 'Артём Басман'
+   return render_template('example.html', name=name)
 
 @app.route("/menu")
 def menu():
