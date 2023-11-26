91325488b1f55d615385f190ffbe4796dcccb447
Артём Басман <steamakk47293@gmail.com>
2023-11-26 15:14:56 +0700

    Авторизация»

--- lab4.py
+++ lab4.py
@@ -5,3 +5,25 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
    return render_template('lab4.html')
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    errors={}
+    if request.method == 'GET':
+        return render_template('login.html', errors=errors)
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+        return render_template('success_2.html', username=username)
+    errors["cred"]= 'Неверные логин и/или пароль' 
+    username= request.args.get('username')
+    if username == '':
+        errors['username']= 'Не введен логин!'
+    if password == '':
+        errors['password']= 'Не введен пароль!'
+    return render_template('login.html', username=username, password=password, errors=errors)
+ 
+@lab4.route('/lab4/success_2')
+def order():
+    return render_template('login.html')
