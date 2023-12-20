497dbcd1910e22c746300c999ef3a7fc2ad86bde
Артём Басман <steamakk47293@gmail.com>
2023-12-21 02:04:25 +0700

    lab6

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
-from flask import Flask, session
+from flask import Flask, url_for, render_template, session
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from flask_sqlalchemy import SQLAlchemy
+from lab6 import lab6
+from Db import db
+from Db.models import users,articles
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+
+
 app.secret_key = "123"
+user_db = "artem_knowledge_base"
+host_ip = "127.0.0.1"
+host_port = "5432"
+database_name = "knowledge_base_for_artem"
+password="123"
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+login_manager = LoginManager()
+
+
+login_manager.login_view = "lab6.login"
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_users(user_id):
+
+    return users.query.get(int(user_id))
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
